99ee9f406ad12e4a4d2b52bc86c6fdeff2582a99
GaetanCSimplon <gcourtieupro@gmail.com>
2025-10-14 16:51:25 +0200

    Maj pred_eolienne : prise en compte des features nécessaires à partir des inputs utilisateurs

--- backend/app/routes/eolienne.py
+++ backend/app/routes/eolienne.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import pandas as pd
 from app.model_trainer import ModelTrain
 
+
 router = APIRouter()
 
 class EolienneInput(BaseModel):
@@ -16,6 +17,10 @@ def predict_wind(data: EolienneInput):
         return {"error": " wind_speed_10m_mean, pressure_msl_mean et temperature_2m_mean devraient être plus nombreux 0"}
     
     df = pd.DataFrame([data.model_dump()])
-    model = ModelTrain.load("eolienne", ["wind_speed_10m_mean", "pressure_msl_mean", "temperature_2m_mean"], "prod_eolienne")
+    # Création des features 
+    df['wind_speed3'] = df['wind_speed_10m_mean']**3
+    df['temp_press'] = df['temperature_2m_mean']*df['pressure_msl_mean']
+    
+    model = ModelTrain.load("eolienne", ["wind_speed_10m_mean", "pressure_msl_mean", "temperature_2m_mean", "wind_speed3", "temp_press"], "prod_eolienne")
     prediction = model.predict(df)[0]
     return {"prediction": prediction}
